05f5fbe6bf8581a9a3ac759a7be29deb89f0d992
Paul Batchelor <thisispaulbatchelor@gmail.com>
2023-04-12 09:48:57 -0400

    added bltriangle example

---
+++ examples/bltriangle.rs
@@ -0,0 +1,34 @@
+// Produces raw floating point samples and writes them to disk
+// convert to WAV using SoX:
+// sox -t raw -r 44100 -e floating-point -c 1 -b 32 test.bin test.wav
+
+use boingboingboing as boing;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut bltriangle = boing::blep(44100);
+    let mut blk: [f32; 64] = [0.0; 64];
+    let mut bytes: [u8; 256] = [0; 256];
+    let nblks = (44100 * 5) / 64;
+    let file = File::create("test.bin");
+
+    bltriangle.set_freq(330.0);
+
+    for _ in 0..nblks {
+        for n in 0..64 {
+            blk[n] = bltriangle.triangle() * 0.3;
+        }
+
+        for n in 0..64 {
+            let pos = n * 4;
+            let b = blk[n].to_le_bytes();
+            bytes[pos] = b[0];
+            bytes[pos + 1] = b[1];
+            bytes[pos + 2] = b[2];
+            bytes[pos + 3] = b[3];
+        }
+
+        _ = file.as_ref().unwrap().write_all(&bytes);
+    }
+}
